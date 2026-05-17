bed752bf5711023e0c11dce3eb48499f619e7e3f
Andriy Fedirko <fedirko816@gmail.com>
2026-05-17 16:35:37 +0300

    Fix CI: add DATABASE_URL default, fix secret name mapping

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ load_dotenv()
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('APP_SECRET_KEY')
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'sqlite:///schedule.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 ADMIN_PIN = os.getenv('ADMIN_PIN')  # Код підтвердження для адміністратора
